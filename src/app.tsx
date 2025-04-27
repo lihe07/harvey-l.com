0cac6d0698f625cb8af395f08d8844fcaeb7256b
lihe07 <li@imlihe.com>
2025-04-27 19:59:57 +0800

    init

--- src/app.tsx
+++ src/app.tsx
@@ -5,18 +5,21 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
+import { MetaProvider } from "@solidjs/meta";
 
 export default function App() {
   return (
-    <Router
-      root={(props) => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <MetaProvider>
+      <Router
+        root={(props) => (
+          <>
+            {/* <Nav /> */}
+            <Suspense>{props.children}</Suspense>
+          </>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </MetaProvider>
   );
 }
