412451238d9595a3dad41de5541dce5c21078b9b
lihe07 <li@imlihe.com>
2025-07-22 11:43:22 -0400

    update

---
+++ src/routes/contact.tsx
@@ -0,0 +1,21 @@
+import PageHead from "~/components/PageHead";
+import { Title } from "@solidjs/meta";
+import "~/assets/styles/markdown.css";
+import Content from "~/components/contact/Content.mdx"
+
+export default () => {
+  return (
+    <main>
+      <Title>harvey-l.com - Contact</Title>
+      <PageHead
+        title="Get in Touch"
+        description="Contact me for any inquiries or just to say hi!"
+      />
+      <div class="px-10 my-10">
+        <article class="markdown-body ma max-w-300 ">
+          <Content />
+        </article>
+      </div>
+    </main>
+  );
+};
