912ac86a42fe0106b493489e0df343676d397d55
lihe07 <li@imlihe.com>
2025-08-05 11:00:58 +0100

    engl

--- src/routes/server/render.tsx
+++ src/routes/server/render.tsx
@@ -2,15 +2,13 @@ import { createAsync, query } from "@solidjs/router";
 import { For } from "solid-js";
 import PageHead from "~/components/PageHead";
 
-import { listBlogQuery } from "~/server";
+import { listBlogQuery, listEnglQuery } from "~/server";
 
 
-export const route = {
-  preload: () => listBlogQuery(),
-};
 
 export default () => {
   const blogList = createAsync(() => listBlogQuery())
+  const englList = createAsync(() => listEnglQuery())
 
   return (
     <div>
@@ -30,6 +28,21 @@ export default () => {
           </p>
         )}
       </For>
+
+      <a href="/server/listEngl">listEngl</a>
+      <For each={englList()}>
+        {e => (
+          <p>
+            <a href={e.href}>
+              {e.title} - {e.date}
+            </a>
+            <a href={`/server/meta/${e.slug}`}>
+              server meta
+            </a>
+          </p>
+        )}
+      </For>
+
     </div >
   )
 }
